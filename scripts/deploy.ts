3c57e0acd88c17cc1bf5b3460ebe0920cf01a618
fai516 <yfsiuv@gmail.com>
2024-02-10 14:45:43 -0500

    upgrade ethers to 6.6.x

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,11 @@
 import hre, { ethers } from "hardhat";
-// import { toWei } from "web3-utils"
 
 async function main() {
   console.log("Selected network: ", hre.network.name);
   const contractName = "XstroStake2Play";
-  // const [owner] = await ethers.getSigners();
-  // const contract = await ethers.deployContract(contractName);
-  const contract = await ethers.deployContract(contractName, [], {});
+  const contract = await ethers.deployContract(contractName);
   await contract.waitForDeployment();
-  console.log(contract);
-
-  // const contract = await ethers.deployContract(contractName, [5]);
-  // console.log(await contract.getAddress());
+  console.log("Address: ", await contract.getAddress());
 }
 
 main().catch((error) => {
